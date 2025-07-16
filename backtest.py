3f25e42db999c1309abcc557b4043216d802ad6c
Utkarsh Shukla <shuklautkarsh1027@gmail.com>
2025-07-16 21:37:36 +0530

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,18 +1,29 @@
-
 def backtest(df, initial_capital=100000):
     capital = initial_capital
     position = 0
+    entry_price = 0
     capital_over_time = []
 
     for i in range(1, len(df)):
-        if df.loc[i-1, 'Signal'] == 1:  # Buy
-            position = capital / df.loc[i, 'Close']
-            capital = 0
-        elif df.loc[i-1, 'Signal'] == -1 and position > 0:  # Sell
-            capital = position * df.loc[i, 'Close']
-            position = 0
-        capital_over_time.append(capital + (position * df.loc[i, 'Close'] if position else 0))
+        price = df.iloc[i]['Close']
+        signal = df.iloc[i - 1]['Signal']
 
-    df = df.iloc[1:]
+        # BUY
+        if signal == 1 and capital > 0:
+            position = capital / price
+            entry_price = price
+            capital = 0
+
+        # SELL
+        elif signal == -1 and position > 0:
+            capital = position * price
+            position = 0
+
+        # Update portfolio value
+        current_value = capital + (position * price if position > 0 else 0)
+        capital_over_time.append(current_value)
+
+    df = df.iloc[1:].copy()
+    df.reset_index(drop=True, inplace=True)
     df['Portfolio_Value'] = capital_over_time
     return df
